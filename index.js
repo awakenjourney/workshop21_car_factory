52a3661ba7b47cdcb59fd8d5c0cbf510eb7f4499
awakenjourney <mayer.sombat@gmail.com>
2023-05-11 18:39:54 -0600

    add pseudo code

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * create Car constructor function with properties of make, model, year
+ * crate getDescription() to Car prototype, then return Car's info as string
+ * use extends to create ElectricCar Function, which is a subclass of Car
+ * overide Car's getDescription() with ElectricCar getDescription()
+ * create instance of ElectricCar with the Car's info: tesla, model s, year 2019, and range 300
+ */
+
 class Car {
   constructor(make, model, year) {
     this.make = make;
